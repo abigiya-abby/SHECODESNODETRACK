15b3514cc28fc7f05ec5385c83ebab687e87c171
Abigiya-abby <Abigiyayonasss@gmail.com>
2026-03-18 19:49:23 +0300

    addC_LALC.js

---
+++ C_ALC1.js
@@ -0,0 +1,10 @@
+let a=10;
+let b=5;
+let sum=a+b;
+let difference= a-b;
+let productt= a*b;
+let division= a/b
+console.log("SUM: ",sum);
+console.log("DIFFERENCE: ", difference);
+console.log("PRODUCT: ",productt);
+console.log("DIVISION: ",division);
